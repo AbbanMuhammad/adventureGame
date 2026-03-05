ca74d0bef42bfe7df046ef272f93975e5ae17ba7
Sani Ismail <saniismail2@gmail.com>
2026-03-05 05:39:14 +0100

    update

--- src/adventureGame.js
+++ src/adventureGame.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// Add a welcome message
-console.log("Prepare yourself for an epic journey!");
-
-=======
 /*
 Adventure Game
 This will be a text-based adventure game where the player will be able to make choices that affect the outcome of the game.
@@ -14,33 +9,3 @@ console.log("Welcome to the Adventure Game!");
 // Add a welcome message
 console.log("Prepare yourself for an epic journey!");
 
->>>>>>> parent of 9caba66 (Added player stats initialization and display code.)
-const readline = require("readline-sync");
-
-// Get player name
-const playerName = readline.question("What's your name, adventure? ");
-console.log(`welcome, ${playerName}! Your adventure begins now.`);
-<<<<<<< HEAD
-// Initialize player stats
-// Information about the player
-=======
-
-// Information about the player 
->>>>>>> parent of 9caba66 (Added player stats initialization and display code.)
-const playerHealth = 100;
-const playerGold = 20;
-const currentLocation = "village";
-const gameRunning = true;
-<<<<<<< HEAD
-// Display player stats
-console.log(`\n=== ${playerName}'s Stats ===`);
-console.log(`Health: ${playerStats.health}`);
-console.log(`Mana: ${playerStats.m
-
-//let inventory = [];
-=======
-
-let inventory = [];
-
-// Create variables for player stats
->>>>>>> parent of 9caba66 (Added player stats initialization and display code.)
